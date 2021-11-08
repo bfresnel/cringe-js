09c7f34857c4db843ca552b8050b46a9ce2dac9d
fresnel <benoit.fresnel.1@gmail.com>
2021-11-09 00:37:34 +0100

    Removed preload script

--- src/main.js
+++ src/main.js
@@ -5,9 +5,6 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-    },
   });
 
   win.loadFile("index.html");
