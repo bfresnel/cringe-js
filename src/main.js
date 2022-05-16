d2495f9f7bffbca875e8b368424102f08c900089
bfresnel <5629642+bfresnel@users.noreply.github.com>
2022-05-16 22:39:30 +0200

    We can now open cringe-js with a specific shortcut

--- src/main.js
+++ src/main.js
@@ -1,22 +1,26 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, globalShortcut } = require("electron");
 
 function createWindow() {
-  const win = new BrowserWindow({
-    width: 300,
-    height: 300,
-    title: 'Cringify',
-    autoHideMenuBar: true,
-  });
+    const win = new BrowserWindow({
+        width: 300,
+        height: 300,
+        title: "Cringify",
+        autoHideMenuBar: true,
+    });
 
-  win.loadFile("index.html");
+    win.loadFile("index.html");
 }
 
-app.whenReady().then(() => {
-  createWindow();
+app.whenReady()
+    .then(() => {
+        globalShortcut.register("Alt+CommandOrControl+I", () => {
+            createWindow();
+        });
+    })
+    .then(createWindow);
 
-  app.on("activate", () => {
+app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
+        createWindow();
     }
-  });
 });
