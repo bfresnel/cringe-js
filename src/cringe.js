bce12edc98e3b658a1fbf48d3342dfb9be64ebc7
bfresnel <5629642+bfresnel@users.noreply.github.com>
2022-05-18 19:46:14 +0200

    Cleaning Import + change var name

--- src/cringe.js
+++ src/cringe.js
@@ -1,27 +1,17 @@
-//import clipboard from 'clipboardy';
-
 function formatToCringeMode(inputIDToCringify) {
-    let cringeLocal = '';
-    let cringifyAreaText = document.getElementById(inputIDToCringify).value;
-    let cringifiedText = document.getElementById('cringifiedText');
-    console.log({ cringifyAreaText });
-    console.log({ cringifiedText });
-    let compteur = 0;
-    for (let index = 0; index < cringifyAreaText.length; index++) {
-        const element = cringifyAreaText[index];
+    let cringifiedText = '';
+    let textToCringify = document.getElementById(inputIDToCringify).value;
+    let inputCringifiedText = document.getElementById('cringifiedText');
+
+    // for each letter of the written text
+    for (let letter of textToCringify) {
         const random = Math.random();
         if (random >= 0.5 || compteur >= 2) {
-            cringeLocal = cringeLocal + element.toUpperCase();
-            compteur = 0;
+            cringifiedText = cringifiedText + letter.toUpperCase();
         } else {
-            cringeLocal = cringeLocal + element.toLowerCase();
-            compteur++;
+            cringifiedText = cringifiedText + letter.toLowerCase();
         }
     }
 
-    // if (clipboardLocal) {
-    //     clipboard.writeSync(cringe);
-    // }
-
-    cringifiedText.value = cringeLocal;
+    inputCringifiedText.value = cringifiedText;
 }
