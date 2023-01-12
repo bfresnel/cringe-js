c9a18cc118ec93d8ccba609643833de690edb35f
bfresnel <5629642+bfresnel@users.noreply.github.com>
2023-01-12 23:29:23 +0100

    Code cleaning

--- src/cringe.js
+++ src/cringe.js
@@ -1,17 +1,13 @@
-function formatToCringeMode(inputIDToCringify) {
-    let cringifiedText = '';
-    let textToCringify = document.getElementById(inputIDToCringify).value;
-    let inputCringifiedText = document.getElementById('cringifiedText');
+function formatToCringeMode(textToCringify) {
+    let result = ''
 
     // for each letter of the written text
     for (let letter of textToCringify) {
-        const random = Math.random();
-        if (random >= 0.5 || compteur >= 2) {
-            cringifiedText = cringifiedText + letter.toUpperCase();
-        } else {
-            cringifiedText = cringifiedText + letter.toLowerCase();
-        }
+        const random = Math.random()
+        random >= 0.5
+            ? (result += letter.toUpperCase())
+            : (result += letter.toLowerCase())
     }
-
-    inputCringifiedText.value = cringifiedText;
+    
+    return result
 }
